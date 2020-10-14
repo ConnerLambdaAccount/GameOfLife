da99d85e9a4f138332249d321abd48f4e1117102
hai666l <0xconr@protonmail.com>
2020-10-13 22:15:41 -0500

    Cell initializer written, abort trap @ setting up neighbors, otherwise method works for adding a cell to the grid

--- cell.c
+++ cell.c
@@ -1,18 +1,45 @@
 struct cell {
 	int x;
 	int y;
-	struct cell* neighbors[8];
+	struct cell** neighbors;
 };
 
 // Adds cell to grid
-// Returns 0 on success -1 on failure
-int newCell(int x, int y, struct cell ***grid) {
+// Will segfault if OOB coordinates provided
+struct cell* newCell(int x, int y, struct cell ***grid) {
 	printf("new cell being created @ grid[%i][%i]\n", y, x);
 	struct cell* neighbors[8];
 	for (int i=0;i<8;i++) {
-		
+		neighbors[i] = NULL;
 	}
-	struct cell newCell = {.x=x, .y=y, .neighbors=neighbors};
-	return 0;	
+	/*
+	int i = 0; // neighbors[] index
+	for (int y_offset=-1;y_offset<=1;y_offset++) {
+		for (int x_offset=-1;x_offset<=1;x_offset++) {
+			// Check neighbor is in bounds of grid
+			if( (y+y_offset >= 0 && y+y_offset <= H-1) && (x+x_offset >= 0 && x+x_offset <= W-1) )
+				neighbors[i] = grid[y+y_offset][x+x_offset];
+			i++;
+			
+			if(i == 4)	// skip self as neighbor
+				i++;
+		}
+	}*/
+		// [0] == [y-1][x-1]
+		// [1] == [y-1][x]
+		// [2] == [y-1][x+1]
+	
+		// [3] == [y][x-1]
+		// [cell] == [y][x]
+		// [4] == [y][x+1]
+		
+		// [5] == [y+1][x-1]
+		// [6] == [y+1][x]
+		// [7] == [y+1][x+1]
+	struct cell *newCell = malloc(sizeof(struct cell));
+	struct cell myCell = {.x=x, .y=y, .neighbors=neighbors};
+	*newCell = myCell;
+	grid[y][x] = newCell;
+	return newCell;	
 }
 
--- gol.c
+++ gol.c
@@ -1,15 +1,14 @@
 #include<stdio.h>
 #include<unistd.h>
 
-#include "grid.c"
-
-#define W 70
 #define H 21
+#define W 70
+
+#include "grid.c"
 
 int main(void) {
 	struct cell*** grid = initGrid(H, W);
-	struct cell cell1 = {.x=5, .y=10, .neighbors=NULL};
-	struct cell cell2 = {.x=6, .y=10, .neighbors=NULL};
+	struct cell *cell = newCell(5, 10, grid);
 	for (int t=0;;t++) {
 		printGrid(grid, H, W);
 		printf("%i Generations\n", t);
