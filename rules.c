d3c5384af736a2751a694694e90a965190cc5390
hai666l <0xconr@protonmail.com>
2020-10-23 18:31:27 -0500

    Revamp rules.c to not include useless rules (1 & 2)

--- rules.c
+++ rules.c
@@ -1,25 +1,4 @@
-/*
-	POPULATED CELLS
-*/
-
-// 1. Each cell with one or no neighbors dies
-void exec_rule1(int y, int x, struct cell*** display, struct cell*** buffer) {
-	if( display[y][x] == NULL)
-		return;
-
-	if( !(neighborCount(y, x, display) <= 1) )
-		newCell(y, x, buffer);
-}
-
-// 2. Each cell with four or more neighbors dies
-void exec_rule2(int y, int x, struct cell*** display, struct cell*** buffer) {
-	if( display[y][x] == NULL)
-		return;
-	if( !(neighborCount(y, x, display) >= 4) )
-		newCell(y, x, buffer);
-}
-
-// 3. Each cell with two or three neighbors survives
+// 3. Each living cell with two or three neighbors survives
 void exec_rule3(int y, int x, struct cell*** display, struct cell*** buffer) {
 	if( display[y][x] == NULL)
 		return;
@@ -29,11 +8,7 @@ void exec_rule3(int y, int x, struct cell*** display, struct cell*** buffer) {
 		newCell(y, x, buffer);
 }
 
-/*
-	UNPOPULATED CELLS
-*/
-
-// 4. Each cell with 3 neighbors becomes alive
+// 4. Each unpopulated cell with 3 neighbors becomes alive
 void exec_rule4(int y, int x, struct cell*** display, struct cell*** buffer) {
 	if( display[y][x] != NULL)
 		return;
