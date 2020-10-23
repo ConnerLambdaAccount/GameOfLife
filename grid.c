7f8f17b5144d13b13b0648b263c93daf1d72fa3c
hai666l <0xconr@protonmail.com>
2020-10-23 18:30:13 -0500

    Revamp grid.c to not use Macro H & W

--- grid.c
+++ grid.c
@@ -1,21 +1,24 @@
-struct cell*** initGrid(void) {
-    struct cell ***grid = malloc( H * sizeof(struct cell**) );
-    for(int y=0;y<H;y++){
-        grid[y] = calloc( W, sizeof(struct cell*) );
+// Initializes a 2D array of (struct cell*) of size h*w in the heap, all pointers -> NULL
+struct cell*** initGrid(int h, int w) {
+    struct cell ***grid = malloc( h * sizeof(struct cell**) );
+    for(int y=0;y<w;y++){
+        grid[y] = calloc( w, sizeof(struct cell*) );
     }
     return grid;
 }
 
-void freeGrid(struct cell*** grid) {
-	for(int y=0;y<H;y++) {
+// Deallocates heap memory of grid
+void freeGrid(int h, struct cell*** grid) {
+	for(int y=0;y<h;y++) {
 		free(grid[y]);
 	}
 	free(grid);
 }
 
-void clearGrid(struct cell*** grid) {
-	for(int y=0;y<H;y++) {
-		for(int x=0;x<W;x++) {
+// Frees all allocated struct cells and sets the pointer to them to NULL
+void clearGrid(int h, int w, struct cell*** grid) {
+	for(int y=0;y<h;y++) {
+		for(int x=0;x<w;x++) {
 			if( grid[y][x] != NULL ) {
 				free(grid[y][x]);
 				grid[y][x] = NULL;
@@ -24,28 +27,17 @@ void clearGrid(struct cell*** grid) {
 	}
 }
 
-void randomizeGrid(struct cell*** grid) {
+// Generates a random grid
+void randomizeGrid(int h, int w, struct cell*** grid) {
 	// Seed rand
 	srand(time(NULL));
-
+	// Clear existing grid
 	clearGrid(grid);
-	for(int y=0;y<H;y++) {
-		for(int x=0;x<W;x++) {
+	for(int y=0;y<h;y++) {
+		for(int x=0;x<w;x++) {
+			// Random num with either be 0 or 1
 			if(rand() % 2 == 1) 
 				newCell(y, x, grid);		
 		}
-	}
-}
-
-void printGrid(struct cell*** grid) {
-	printf("\e[1;1H\e[2J"); 
-	for (int y=0;y<H;y++) {
-		for (int x=0;x<W;x++) {
-			if (grid[y][x] != NULL)
-				printf("#");
-			else
-				printf("-");
-		}
-		printf("\n");
 	}
 }
